9821fdc4b91940baed8adde83990a06f117dbf69
Zzz <Zzz@LAPTOP-SGEDL99B>
2023-03-09 10:55:58 +0700

    Add project files.

---
+++ HoangTienDung_2080600008_BigSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HoangTienDung_2080600008_BigSchool.MvcApplication" Language="C#" %>
---
+++ HoangTienDung_2080600008_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HoangTienDung_2080600008_BigSchool.Startup))]
+namespace HoangTienDung_2080600008_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
